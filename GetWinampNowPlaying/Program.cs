ae32913654235d385928f878b792f4323f858a2d
Robert Whitney <xnite@xnite.me>
2017-02-25 16:19:47 -0600

    initial commit

---
+++ GetWinampNowPlaying/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace GetWinampNowPlaying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process process in processlist)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    if(process.ProcessName == "winamp")
+                    {
+                        string returnData = "Now playing: " + process.MainWindowTitle.Replace(" - Winamp", "");
+                        Console.WriteLine(returnData);
+                    }
+                }
+            }
+        }
+    }
+}
